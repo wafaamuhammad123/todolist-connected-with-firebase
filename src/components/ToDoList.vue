02d7c7ab5692603dc295fcb0f1dad5279e5cf0d8
wafaamuhammad123 <wafaa.muhammad2000@gmail.com>
2024-08-04 21:44:01 +0300

    edit task

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,23 +1,22 @@
 <template>
-	<div class="wrapper">
-		<h2>
-			To do list
-		</h2>
-
-		<div class="list">
-			<input type="text" v-model="newTask" placeholder="add task" class="task"
-				@keyup.enter="addTask">
-			<button @click=" clearTasks" class="clear">delete all</button>
-		</div>
-
-		<!-- store the list of tasks -->
-		<h4 class="task2" v-for="task in taskStore.tasks" :class=" { completed: task.completed } "
-			@click="Toggle( task.id )"  :key="task.id">{{task.title}}
-			<button @click="deleteTask(task.id)" class="delete">delete</button>
-		</h4>
-
-		<!-- const task = tasks.value.find(t => t.id === taskId); -->
-	</div>
+  <div class="wrapper">
+    <h2>To do list</h2>
+    <div class="list">
+      <input type="text" v-model="newTask" placeholder="add task" class="task" @keyup.enter="addTask">
+      <button @click="clearTasks" class="clear">Delete all</button>
+    </div>
+    <div v-for="task in taskStore.tasks" :key="task.id" class="task-item">
+      <div v-if="editTaskId === task.id">
+        <input v-model="editedTaskTitle" @keyup.enter="updateTask(task.id)">
+        <button @click="cancelEdit">Cancel</button>
+      </div>
+      <div v-else>
+        <h4 :class="{ completed: task.completed }" @click="Toggle(task.id)">{{ task.title }}</h4>
+        <button class="edit" @click="startEdit(task.id, task.title)">Edit</button>
+        <button @click="deleteTask(task.id)" class="delete">Delete</button>
+      </div>
+    </div>
+  </div>
 </template>
 <!-- <script src="firebase.js"></script> -->
 <script>
@@ -27,7 +26,8 @@ export default {
 	setup () {
 		const taskStore = useTaskStore() //initializes the task store.
 		const newTask = ref( '' ) //  Creates a reactive reference for the new task input.
-
+    const editTaskId = ref(null);
+    const editedTaskTitle = ref('');
 		
 		function addTask () {
 			// handles the local logic related to the user interface, such as reading the input value and interacting with the store
@@ -44,8 +44,31 @@ export default {
 
 			taskStore.isCompleted( taskId )
 		}
+	
+	
 
-		return { taskStore, newTask, addTask ,clearTasks:taskStore.clearTasks, deleteTask:taskStore.deleteTask, Toggle} //to make them available in the template
+    function startEdit(taskId, title) {
+      editTaskId.value = taskId;
+      editedTaskTitle.value = title;
+    }
+
+    function cancelEdit() {
+      editTaskId.value = null;
+      editedTaskTitle.value = '';
+    }
+
+    function updateTask(taskId) {
+      if (editedTaskTitle.value !== '') {
+        taskStore.editTask(taskId, editedTaskTitle.value);
+        cancelEdit();
+      }
+    }
+
+		return { taskStore, newTask, addTask ,clearTasks:taskStore.clearTasks, deleteTask:taskStore.deleteTask, Toggle, startEdit,
+      updateTask,
+      cancelEdit,
+      editTaskId,
+      editedTaskTitle} //to make them available in the template
 	}
 	// fetch ('url', {
 	// 	method: 'POST',
@@ -55,29 +78,34 @@ export default {
 </script>
 
 <style scoped>
-.wrapper{
-			border: 1px solid black;
-					width: 55%;
-						margin-left: 20%;
-					padding: 2%;
-			
+.wrapper {
+  border: 1px solid black;
+  width: 55%;
+  margin-left: 20%;
+  padding: 2%;
 }
 .completed {
-	text-decoration: line-through;
+  text-decoration: line-through;
 }
-.clear{
-	width: 20%;
+.clear {
+  width: 20%;
   font-size: 17px;
   padding: 1.5%;
   background: red;
   border: none;
   color: white;
 }
-.clear:hover{
- background: rgb(231, 4, 4);
+input {
+  width: 30%;
+  padding: 2%;
+  font-size: 17px;
+  border: 1px solid #e9e9e9;
+}
+.clear:hover {
+  background: rgb(231, 4, 4);
 }
 .delete {
-			padding: 2%;
+  padding: 2%;
   margin-left: 2%;
   font-size: 17px;
   background-color: red;
@@ -86,14 +114,26 @@ export default {
   color: white;
   width: 23%;
 }
-.delete:hover{
- background: rgb(231, 4, 4);
+.edit {
+  padding: 2%;
+  margin-left: 2%;
+  font-size: 17px;
+  background-color: green;
+  border: none;
+  border-radius: 7px;
+  color: white;
+  width: 23%;
 }
-.task2 {
-	width: 50%;
+.delete:hover {
+  background: rgb(231, 4, 4);
+}
+.edit:hover {
+  background-color: rgb(5, 107, 5);
+}
+.task-item {
+  width: 50%;
   padding: 2%;
   border: 1px solid #ffeded;
-  /* background-color: #ffce93; */
   margin-left: 20%;
   color: black;
 }
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -1,122 +1,91 @@
-import {
-	defineStore
-} from 'pinia' //to define a new store
-import {
-	ref
-} from 'vue'
-// import {
-// 	collection,
-// 	addDoc,
-// 	getDocs,
-// 	deleteDoc,
-// 	doc,
-// 	updateDoc
-// } from 'firebase/firestore';
-// import {
-// 	db
-// } from '@/firebase';
-export const useTaskStore = defineStore( 'taskStore', () => { //fn define and returns the store
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
 
-	const tasks = ref( [] )
+export const useTaskStore = defineStore('taskStore', () => {
+  const tasks = ref([]);
 
-//now i want to save the tasks in the ui after refreshing
-// =>fetch tasks from firebase when comp is mounted.. then update the array
-//tell him after adding data..push it to the backend
-// add the fn that fetches data from firebase..then call it when comp is mounted
-function fetchTasks() {
-	fetch("https://todolist-2e670-default-rtdb.firebaseio.com/todolist.json")
-		.then(response => response.json())
-		.then(data => {
-			if (data) {
-				tasks.value = Object.keys(data).map(key => ({
-					id: key,
-					title: data[key].title,
-					// completed: data[key].completed || false
-				}));
-			}
-		})
-		.catch(error => console.error(error));
-}
+  function fetchTasks() {
+    fetch("https://todolist-23978-default-rtdb.asia-southeast1.firebasedatabase.app/todolist.json")
+      .then(response => response.json())
+      .then(data => {
+        if (data) {
+          tasks.value = Object.keys(data).map(key => ({
+            id: key,
+            title: data[key].title,
+          }));
+        }
+      })
+      .catch(error => console.error(error));
+  }
 
+  function addTask(task) {
+    const id = Date.now().toString();
+    tasks.value.push({
+      id,
+      title: task,
+    });
+    fetch("https://todolist-23978-default-rtdb.asia-southeast1.firebasedatabase.app/todolist.json", {
+      method: 'POST',
+      body: JSON.stringify({
+        id,
+        title: task,
+      })
+    }).then(response => console.log(response))
+      .catch(error => console.error(error));
+  }
 
-	function addTask( task ) {
-		// handles the logic of adding a task to the list of tasks
-		//function that takes a task string, creates a task object with a unique id and title, and adds it to the tasks array.
-		const id = Date.now()
-		tasks.value.push( {
-			id,
-			title: task,
-			// completed: false
-		} )
-		fetch( "https://todolist-2e670-default-rtdb.firebaseio.com/todolist.json", {
-				method: 'POST',
-				// headers: {
-				// 	'content-Type': 'application/json'
-				// },
-				body: JSON.stringify( {
-					id,
-					title: task,
-					// completed: false
-					// took the obj we pushed in view..to be pushed in bkend
-				} )
-			} ).then( response => console.log( response ) )
-			.catch( error => console.error( error ) )
-			.then( data => console.log( data ) )
-	}
+  function clearTasks() {
+    tasks.value = [];
+    fetch("https://todolist-23978-default-rtdb.asia-southeast1.firebasedatabase.app/todolist.json", {
+      method: 'DELETE',
+    }).then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to delete tasks from backend');
+      }
+    })
+    .catch(error => console.error(error));
+  }
 
-	function clearTasks() {
-		tasks.value = []
-		fetch( "https://todolist-2e670-default-rtdb.firebaseio.com/todolist.json", {
-			method: 'DELETE',
-			// headers: {
-			// 	'content-Type': 'application/json'
-			// },
-			
-		} ).then(response => {
-			if (!response.ok) {
-				throw new Error('Failed to delete task from backend');
-			}
-		})
-		.catch(error => console.error(error));
-	}
-	
-// need to send delete req to firebase
-function deleteTask( taskId ) {
-	tasks.value = tasks.value.filter( task => task.id !== taskId ) //h remove task d with its id from the arr
-	
-	fetch( `https://todolist-2e670-default-rtdb.firebaseio.com/todolist/${taskId}.json`, { 
-		method: 'DELETE',
-		// headers: {
-		// 	'content-Type': 'application/json'
-		// },
-		
-	} ).then(response => {
-		if (!response.ok) {
-			throw new Error('Failed to delete task from backend');
-		}
-	})
-	.catch(error => console.error(error));
-}
+  function deleteTask(taskId) {
+    tasks.value = tasks.value.filter(task => task.id !== taskId);
+    fetch(`https://todolist-23978-default-rtdb.asia-southeast1.firebasedatabase.app/todolist/${taskId}.json`, {
+      method: 'DELETE',
+    }).then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to delete task from backend');
+      }
+    })
+    .catch(error => console.error(error));
+  }
 
+  function editTask(taskId, newTitle) {
+    const task = tasks.value.find(task => task.id === taskId);
+    if (task) {
+      task.title = newTitle;
+      fetch(`https://todolist-23978-default-rtdb.asia-southeast1.firebasedatabase.app/todolist/${taskId}.json`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+          title: newTitle,
+        })
+      }).then(response => console.log(response))
+        .catch(error => console.error(error));
+    }
+  }
 
+  function isCompleted(taskId) {
+    const task = tasks.value.find(task => task.id === taskId);
+    if (task) {
+      task.completed = !task.completed;
+    }
+  }
 
-
-	function isCompleted( taskId ) {
-		//find the task.id=>line through
-		const task = tasks.value.find( task => task.id === taskId );
-		if ( task ) {
-			task.completed = !task.completed
-		}
-
-	}
-	return {
-		tasks,
-		addTask,
-		fetchTasks,
-		clearTasks,
-		deleteTask,
-		isCompleted
-	}
-
-
-} )
+  return {
+    tasks,
+    addTask,
+    fetchTasks,
+    clearTasks,
+    deleteTask,
+    editTask,
+    isCompleted
+  };
+});
